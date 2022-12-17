66be89f648b319deea7e408627352455d9597878
aadamGithub <aadamxxmail@gmail.com>
2022-12-17 01:21:39 +0000

    fixing django secret key

--- aalupino/settings.py
+++ aalupino/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b^s1_jl)#y5i&&$o%!v45r(h%u!jfgr7&iz+owm$$n*71zhtx0'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
